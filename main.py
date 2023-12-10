c472ac129cf034c49e80ec27572cfb593fbf9ab7
Nikita <barskii.no@outlook.com>
2023-12-10 19:54:22 +0300

    Add filename as parameter, fix get_age_writing function

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas
 import collections
+import argparse
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from datetime import datetime
 
 
 def get_age_writing(age):
-    age_exceptions = [11, 12, 13, 14, 111, 913, 2012]
-    if age in age_exceptions:
+    if age % 100 in [11, 12, 13, 14]:
         return "лет"
     if age % 10 == 1:
         return "год"
@@ -17,6 +17,14 @@ def get_age_writing(age):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script for Winery Site')
+    parser.add_argument(
+        'file',
+        nargs='?',
+        default='wine_template.xlsx',
+        help='Enter excel filename'
+    )
+    args = parser.parse_args()
     env = Environment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -31,18 +39,18 @@ if __name__ == '__main__':
         'Картинка': 'image',
         'Акция': 'special_offer'
     }
-    read_wine_excel = pandas.read_excel(
-        'wine3.xlsx',
+    wine_excel = pandas.read_excel(
+        args.file,
         na_values=' ',
         keep_default_na=False
     ).rename(columns=new_column_names)
     wine_categories = collections.defaultdict(list)
-    for wine in read_wine_excel.to_dict('records'):
+    for wine in wine_excel.to_dict('records'):
         wine_categories[wine['category']].append(wine)
     rendered_index_page = index_template.render(
         age=winery_age,
         age_writing=get_age_writing(winery_age),
-        wine_cards=read_wine_excel.to_dict('records'),
+        wine_cards=wine_excel.to_dict('records'),
         wine_types=wine_categories
     )
     with open('index.html', 'w', encoding="utf8") as file:
